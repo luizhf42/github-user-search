1ab00c82fc0cd22c8f44ef73dbfbfe0ec14946dd
Luiz Henrique <luizhf42@gmail.com>
2021-12-27 14:59:41 -0300

    finished the script
    the code is a mess but it works

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,8 +1,20 @@
-const githubUsernameRegex = /^[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}$/i;
-const searchBtn = document.querySelector(".search-btn");
-const searchInput = document.querySelector(".user-search");
-const userInfoSection = document.querySelector(".user-info");
-const beforeSearchText = document.querySelector(".before-search-text");
+const githubUsernameRegex = /^[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}$/i,
+  searchBtn = document.querySelector(".search-btn"),
+  searchInput = document.querySelector(".user-search"),
+  userInfoSection = document.querySelector(".user-info"),
+  beforeSearchText = document.querySelector(".before-search-text"),
+  avatarElement = document.querySelector(".user-pic"),
+  nameElement = document.querySelector(".name"),
+  usernameElement = document.querySelector(".username"),
+  bioElement = document.querySelector(".text-bio"),
+  reposElement = document.querySelector(".repos-info"),
+  hireableElement = document.querySelector(".hireable-info"),
+  followersElement = document.querySelector(".followers-info"),
+  followingElement = document.querySelector(".following-info"),
+  companyElement = document.querySelector(".company"),
+  locationElement = document.querySelector(".location"),
+  websiteElement = document.querySelector(".website"),
+  twitterUsernameElement = document.querySelector(".twitter");
 
 const validateUsername = (searchedUser) => {
   return githubUsernameRegex.test(searchedUser);
@@ -12,23 +24,118 @@ const makeRequest = (searchedUser) => {
   axios
     .get(`https://api.github.com/users/${searchedUser}`)
     .then((response) => {
-      const {
-        login,
-        name,
-        avatar_url,
-        html_url,
-        followers,
-        following,
-        company,
-        blog,
-        twitter_username,
-        location,
-        bio,
-      } = response.data;
+      const userData = getData(response);
+      showData(userData);
     })
     .catch((error) => {
       console.log(error);
     });
+};
+
+const getData = (response) => {
+  const {
+    avatar_url,
+    html_url,
+    name,
+    login,
+    bio,
+    public_repos,
+    hireable,
+    followers,
+    following,
+    company,
+    location,
+    twitter_username,
+    blog,
+  } = response.data;
+
+  const userData = {
+    avatar_url,
+    html_url,
+    name,
+    login,
+    bio,
+    public_repos,
+    hireable,
+    followers,
+    following,
+    company,
+    location,
+    twitter_username,
+    blog,
+  };
+
+  return userData;
+};
+
+const showData = (userData) => {
+  const {
+    avatar_url,
+    html_url,
+    name,
+    login,
+    bio,
+    public_repos,
+    hireable,
+    followers,
+    following,
+    company,
+    location,
+    twitter_username,
+    blog,
+  } = userData;
+
+  const elements = [
+    avatarElement,
+    nameElement,
+    usernameElement,
+    bioElement,
+    reposElement,
+    hireableElement,
+    followersElement,
+    followingElement,
+    companyElement,
+    locationElement,
+    websiteElement,
+    twitterUsernameElement,
+  ];
+
+  avatarElement.src = avatar_url;
+
+  nameElement.textContent = name;
+  nameElement.href = html_url;
+
+  usernameElement.textContent = login;
+
+  bioElement.textContent = bio;
+
+  reposElement.textContent = public_repos;
+
+  if (hireable === true) {
+    hireableElement.textContent = "Yes";
+  } else {
+    hireableElement.textContent = "No";
+
+  }
+
+  followersElement.textContent = followers;
+
+  followingElement.textContent = following;
+
+  companyElement.textContent = company;
+
+  locationElement.textContent = location;
+
+  twitterUsernameElement.textContent = twitter_username;
+
+  websiteElement.textContent = blog;
+
+  elements.forEach((element) => {
+    if (element.textContent === "" || element.textContent === null) {
+      element.textContent = "No info";
+      element.classList.add('no-info');
+    }
+  })
 };
 
 searchBtn.addEventListener("click", (event) => {
@@ -40,8 +147,10 @@ searchBtn.addEventListener("click", (event) => {
 
   if (usernameIsValid) {
     searchInput.classList.remove("error");
+    beforeSearchText.style.display = "none";
     makeRequest(searchedUser);
-    // show the info
+    userInfoSection.style.visibility = "visible";
+
   } else {
     userInfoSection.style.visibility = "hidden";
     beforeSearchText.textContent = "Invalid username!";
